bccee603692a9651c5e27dddf01711a09f0c976e
Camila Lordelo <camilalordelo15@gmail.com>
2023-11-11 22:03:56 -0300

    feat: search function

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -9,17 +9,25 @@ import { CharacterService } from 'src/app/services/character.service';
 })
 export class CharactersComponent implements OnInit {
 
+  public allCharacters: Array<Character> = []
   public characters: Array<Character> = []
+  public searchTerm: string = '';
 
   constructor(private characterService: CharacterService) {}
 
   ngOnInit(): void {
     this.characterService.getAllCharacters().subscribe((data) => {
+      this.allCharacters = data.results;
       this.characters = data.results;
     });
   }
 
-  search(event: any): void {
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
 
+    this.characters = this.allCharacters.filter((character) => {
+      return character.name.toLowerCase().includes(value)
+    });
   }
 }
