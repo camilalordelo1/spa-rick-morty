de1264e3ec74c8f2cb05cb8200c7455e8b48e4ce
Camila Lordelo <camilalordelo15@gmail.com>
2023-11-13 17:39:01 -0300

    feat: concomitants filters character

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -9,9 +9,13 @@ import { CharacterService } from 'src/app/services/character.service';
 })
 export class CharactersComponent implements OnInit {
 
-  public allCharacters: Array<Character> = []
-  public characters: Array<Character> = []
+  public allCharacters: Array<Character> = [];
+  public characters: Array<Character> = [];
   public searchTerm: string = '';
+
+  valueFilterSearch = '';
+  valueFilterStatus = '0';
+  valueFilterSpecie = '0';
 
   constructor(private characterService: CharacterService) {}
 
@@ -22,29 +26,71 @@ export class CharactersComponent implements OnInit {
     });
   }
 
-  search(e: Event): void {
+  filterBySearch(e: Event): void {
     const target = e.target as HTMLInputElement;
     const value = target.value.toLowerCase();
+    this.valueFilterSearch = value;
 
-    this.characters = this.allCharacters.filter((character) => {
-      return character.name.toLowerCase().includes(value)
-    });
+    this.loadByFilters();
   }
 
   filterBySpecie(e: Event): void {
     const target = e.target as HTMLInputElement;
     const value = target.value;
+    this.valueFilterSpecie = value;
 
-    if(value != '0'){
-      this.characters = this.allCharacters.filter((character) => {
-        return character.species.includes(value)
-      });
-    } else {
-      this.characters = this.allCharacters;
-    }
+    this.loadByFilters();
   }
 
   filterByStatus(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+    this.valueFilterStatus = value;
 
+    this.loadByFilters();
   }
+
+  loadByFilters(): void {
+    if(this.valueFilterSearch != '' && this.valueFilterSpecie == '0' && this.valueFilterStatus == '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.name.toLowerCase().includes(this.valueFilterSearch)
+      });
+    } else if (this.valueFilterSearch == '' && this.valueFilterSpecie != '0' && this.valueFilterStatus == '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.species.includes(this.valueFilterSpecie)
+      });
+    } else if (this.valueFilterSearch == '' && this.valueFilterSpecie == '0' && this.valueFilterStatus != '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.status.includes(this.valueFilterStatus)
+      });
+    } else if (this.valueFilterSearch != '' && this.valueFilterSpecie != '0' && this.valueFilterStatus != '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.species.includes(this.valueFilterSpecie) &&
+              character.name.toLowerCase().includes(this.valueFilterSearch) &&
+              character.status.includes(this.valueFilterStatus)
+      });
+    } else if (this.valueFilterSearch != '' && this.valueFilterSpecie != '0' && this.valueFilterStatus == '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.species.includes(this.valueFilterSpecie) &&
+              character.name.toLowerCase().includes(this.valueFilterSearch)
+      });
+    } else if (this.valueFilterSearch != '' && this.valueFilterSpecie == '0' && this.valueFilterStatus != '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.status.includes(this.valueFilterStatus) &&
+              character.name.toLowerCase().includes(this.valueFilterSearch)
+      });
+    } else if (this.valueFilterSearch == '' && this.valueFilterSpecie != '0' && this.valueFilterStatus != '0'){
+      this.characters = this.allCharacters.filter((character) => {
+        return character.status.includes(this.valueFilterStatus) &&
+              character.species.includes(this.valueFilterSpecie)
+      });
+    } else if (this.valueFilterSearch == '' && this.valueFilterSpecie == '0' && this.valueFilterStatus == '0') {
+      this.loadAllCharacters();
+    }
+  }
+
+  loadAllCharacters(): void {
+    this.characters = this.allCharacters;
+  }
+
 }
